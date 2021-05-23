7992b0f9b3168f49e3481f02f3567cf3d7833379
sruthykor <sruthykor@gmail.com>
2021-05-23 16:07:23 +1000

    Updates

---
+++ precache-manifest.fddf33224e14040797b86bb03f51ee35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcbbdbcfd3180cc944ffe593d07075f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a8b161ed5f1dbf8c7ba",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "c3c66076c0617d534a50",
+    "url": "/static/css/main.551b4d68.chunk.css"
+  },
+  {
+    "revision": "5a8b161ed5f1dbf8c7ba",
+    "url": "/static/js/2.d5355a42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d5355a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c66076c0617d534a50",
+    "url": "/static/js/main.26647144.chunk.js"
+  },
+  {
+    "revision": "7dbaecb9b1566e9ab509",
+    "url": "/static/js/runtime-main.9847c3b2.js"
+  }
+]);
